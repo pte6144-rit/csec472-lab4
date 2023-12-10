09c3b12593d9b769256eb7abebe884b1c2be3a6d
taw7879 <taw7879@rit.edu>
2023-12-09 21:47:26 -0500

    new version

--- orb.py
+++ orb.py
@@ -1,12 +1,56 @@
 # Uses orb method
 
-import numpy as np
-import pandas as pd
 import cv2
-import matplotlib.pyplot as plt
 import os
-import shutil
-from PIL import Image
+import random
+import numpy as np
+
+def dumpfile(file):
+    with open(file) as fd:
+        return fd.read()
+    
+def shuffle_files(dir):
+    firstImages = []
+    firstText = []
+    secondImages = []
+    secondText = []
+    firstLetters = []
+    secondLetters = []
+    data = []
+    to_shuffle = []
+    for file in os.listdir(dir):
+        if file.endswith('.png'):
+            if file[0] == "s":
+                secondImages.append(file)
+                secondLetters.append(file[7])
+            else:
+                firstImages.append(file)
+                firstLetters.append(file[7])
+        else:
+            if file[0] == "f":
+                firstText.append(file)
+            else:
+                secondText.append(file)
+    for i in range(len(firstImages)):
+        line = {"fi": os.path.join(dir, firstImages[i]), "ft": dumpfile(os.path.join(dir, firstText[i])),
+                "si": os.path.join(dir, secondImages[i]), "st": dumpfile(os.path.join(dir, secondText[i])),
+                "fl": firstLetters[i], "sl": secondLetters[i]}
+        data.append(line)
+    for i in range(len(data)-1, -1, -1):
+        if random.randrange(2):
+            to_shuffle.append(data[i])
+            del data[i]
+        else:
+            obj = data[i]
+            obj["real"] = True
+            data[i] = obj
+    copy = to_shuffle.copy()
+    for i in range(len(copy)):
+        opposing = random.randrange(len(copy))
+        data.append({"fi": to_shuffle[i]["fi"], "ft": to_shuffle[i]["ft"], "fl": to_shuffle[i]["fl"],
+                     "si": copy[opposing]["si"], "st": copy[opposing]["st"], "sl": copy[opposing]["sl"], "real": False})
+        del copy[opposing]
+    return data
 
 def preprocess_fingers(orb, fingers):
     descriptors = {}
@@ -16,91 +60,92 @@ def preprocess_fingers(orb, fingers):
     return descriptors
 
 def get_finger_features(orb, finger):
-    image = cv2.imread(finger)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.imread(finger, cv2.IMREAD_GRAYSCALE)
+    image = cv2.GaussianBlur(image, (5, 5), 0)
+    image = cv2.equalizeHist(image)
+    
+    image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+
+    kernel = np.ones((3,3), np.uint8)
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+
+    sharpening_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+    image = cv2.filter2D(image, -1, sharpening_kernel)
 
     features, des = orb.detectAndCompute(image, None)
     return des
 
-def compare_fingers(orb, train_fingers, test_fingers, train_descriptors, test_descriptors):
 
-    positives = 0
-    negatives = 0
-    falsePositives = 0
-    falseNegatives = 0
-    for train_finger in train_fingers:
-        match = False
+def compare_fingers(data, set1_descriptors, set2_descriptors, threshold=1000):
+    acceptance = 0
+    insult = 0
+    fraud = 0
+    rejection = 0
 
-        for test_finger in test_fingers:
+    for datum in data:
+        des1 = set1_descriptors[datum["fi"]]
+        des2 = set2_descriptors[datum["si"]]
 
-            des1 = train_descriptors[train_finger]
-            des2 = test_descriptors[test_finger]
-            
-            bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-            matches = bf.match(des1, des2)
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+        matches = bf.match(des1, des2)
 
-            count = len(matches)
-    
-            if count > 200:
-                match = True
-                positives += 1
-                print("Positive: ", positives)
-                print("Negatives: ", negatives)
+        count = len(matches)
+        is_real_match = datum["real"]
 
-        if match == False:
-            negatives += 1
-                
-        
-    return positives, negatives, falsePositives, falseNegatives
-
-# function for getting all the images
-def get_fingers():
-    f_train_fingers = []
-    f_test_fingers = []
-    s_train_fingers = []
-    s_test_fingers = []
-
-    train = "train"
-    test = "test"
-
-    for file in os.listdir(train):
-        if file.endswith('.png'):
-            if file[0] == "s":
-                fingerPath = os.path.join(train, file)
-                s_train_fingers.append(fingerPath)
+        if count > threshold:
+            if is_real_match:
+                acceptance += 1
             else:
-                fingerPath = os.path.join(train, file)
-                f_train_fingers.append(fingerPath) 
-    
-    for file in os.listdir(test):
-        if file.endswith('.png'):
-            if file[0] == "s":
-                fingerPath = os.path.join(test, file)
-                s_test_fingers.append(fingerPath)
+                fraud += 1
+        else:
+            if is_real_match:
+                insult += 1
             else:
-                fingerPath = os.path.join(test, file)
-                f_test_fingers.append(fingerPath)
+                rejection += 1
 
-    return f_train_fingers, f_test_fingers, s_train_fingers, s_test_fingers
-
+    return acceptance, insult, fraud, rejection
 
 def main():
-    print("Starting training")
+    orb = cv2.ORB_create(nfeatures=3000, scaleFactor=1.2)
 
-    orb = cv2.ORB_create(nfeatures=500, scaleFactor=1.2)
-    f_train_fingers, f_test_fingers, s_train_fingers, s_test_fingers = get_fingers()
-    f_train_descriptors = preprocess_fingers(orb, f_train_fingers)
-    #f_test_descriptors = preprocess_fingers(orb, f_test_fingers)
-    s_train_descriptors = preprocess_fingers(orb, s_train_fingers)
-    #s_test_descriptors = preprocess_fingers(orb, s_test_fingers)
+    #print("Starting Training")
+    #training_data = shuffle_files("train")
+    #f_train_fingers = [data["fi"] for data in training_data]
+    #s_train_fingers = [data["si"] for data in training_data]
+    #f_train_descriptors = preprocess_fingers(orb, f_train_fingers)
+    #s_train_descriptors = preprocess_fingers(orb, s_train_fingers)
 
-    print("Start comparison")
-    # Start comparison
-    positives, negatives, falsePositives, falseNegatives = compare_fingers(orb, f_train_fingers, s_train_fingers, f_train_descriptors, s_train_descriptors)
-    #positives, negatives, falsePositives, falseNegatives = compare_fingers(orb, s_train_fingers, s_test_fingers, s_train_fingers_class, s_test_fingers_class)
+    #acceptance, insult, fraud, rejection = compare_fingers(training_data, f_train_descriptors, s_train_descriptors)
+    #print("Training Results: Acceptance:", acceptance, "Insult:", insult, "Fraud:", fraud, "Rejection:", rejection)
 
-    print("Test Results:")
-    print(positives, negatives)
+    print("\nStart Testing")
+    testing_data = shuffle_files("test")
+    f_test_fingers = [data["fi"] for data in testing_data]
+    s_test_fingers = [data["si"] for data in testing_data]
+    f_test_descriptors = preprocess_fingers(orb, f_test_fingers)
+    s_test_descriptors = preprocess_fingers(orb, s_test_fingers)
 
+    #acceptance_test, insult_test, fraud_test, rejection_test = compare_fingers(testing_data, f_test_descriptors, s_test_descriptors)
+    #print("Test Results: Acceptance:", acceptance_test, "Insult:", insult_test, "Fraud:", fraud_test, "Rejection:", rejection_test)
+
+    best_threshold = 900
+    best_difference = float('inf')
+
+    for threshold in range(900, 1000, 10):  # Example range and step
+        acceptance, insult, fraud, rejection = compare_fingers(testing_data, f_test_descriptors, s_test_descriptors, threshold)
+        difference = abs(200 - acceptance) + abs(200 - rejection)
+
+        if difference < best_difference:
+            best_difference = difference
+            best_threshold = threshold
+            acceptance_test = acceptance
+            insult_test = insult
+            fraud_test = fraud
+            rejection_test = rejection
+
+    print("Best Threshold:", best_threshold)
+    print("Test Results: Acceptance:", acceptance_test, "Insult:", insult_test, "Fraud:", fraud_test, "Rejection:", rejection_test)
 if __name__ == "__main__":
     main()
+
+
